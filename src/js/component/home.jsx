0a41df909ef3426532e6ed1658e14a1daff89924
adiaz980 <141368651+adiaz980@users.noreply.github.com>
2023-11-02 00:48:41 +0000

    Updates

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
+import "./app.css";
 
 //create your first component
-const Home = () => {
+function TodoItem ({ label, is_done, delete_todo, toggle_todo }) {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="todoitem">
+					<input type="checkbox" checked={is_done} onChange={toggle_todo} />
+					<span className="todotext">{label}</span>
+				<button className="btn btn-danger" onClick={delete_todo}>Completed</button>
+				</div>
 	);
 };
 
-export default Home;
+function App() {
+	const [todos, setTodos] = useState([]);
+	// const [item, setItem] = useState ({
+	// 	label: "Buy some tuna cans."
+	// 	is.done: false,
+	// });
+	
+	return (
+		<>
+			<form className="container d-flex flex-column align-items-center justify-content-start">
+				<h1>To Do List</h1>
+				<input 
+				className="form-control form-control-lg" 
+				type="text" 
+				placeholder="What do you want to get done today?" 
+				aria-label="input field">
+				</input>
+				{todos.map((item) => (
+					<TodoItem 
+						label= {item.label} 
+						is_done={item.is_done} 
+						toggle_todo={() => 
+					setTodos(		
+					todos.toSpliced(idx, 1, {
+					label: item.label,
+					is_done: !item.is_done,
+				})
+				)
+				}
+				/>
+				// <TodoItem label="Open tuna cans." is.done={false} />
+				// <TodoItem label="Feed cats." is.done={false} /> */}
+				// {/* <div className="todoitem">
+				// 	<input type="checkbox" />
+				// 	<span className="todotext">Feed the cat</span>
+				// <button className="btn btn-danger">Completed</button>
+				// </div> */}
+				))}
+			</form>
+		</>
+	);
+};
+
+export default App;
